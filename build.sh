b2cdca558da006460bc83eb1e3a661016796d0c6
blueback <bluebackpandey@gmail.com>
2025-05-11 08:44:31 +0000

    refactor

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 export JAVA_HOME=jdk-24.0.1
-source setup.sh
+export ANDROID_HOME=cmdline-tools/
+export PATH=${PATH}:${ANDROID_HOME}/enumlator
+export PATH=${PATH}:${ANDROID_HOME}/tools
+export PATH=${PATH}:${ANDROID_HOME}/tools/bin
+export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+
 #cmdline-tools/bin/sdkmanager --install "platform-tools" "platforms;android-34" "build-tools;34.0.0" --sdk_root=cmdline-tools
 #cmdline-tools/bin/sdkmanager --install "gradle" --sdk_root=cmdline-tools
 
