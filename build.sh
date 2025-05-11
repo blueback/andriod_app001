1957345689d0d5338524d43588f3bafb05ab06b3
blueback <bluebackpandey@gmail.com>
2025-05-11 09:29:45 +0000

    small fix

--- build.sh
+++ build.sh
@@ -9,9 +9,10 @@ export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 #./third_party/cmdline-tools/bin/sdkmanager --install "platform-tools" "platforms;android-34" "build-tools;34.0.0" --sdk_root=cmdline-tools
 #./third_party/cmdline-tools/bin/sdkmanager --install "gradle" --sdk_root=cmdline-tools
 
-#./third_party/gradle-8.14/bin/gradle build --stacktrace --info --scan --debug
 rm -rf .gradle
 rm -rf build
 rm -rf app/build
+
+#./third_party/gradle-8.14/bin/gradle build --stacktrace --info --scan --debug
 ./third_party/gradle-8.14/bin/gradle build
 
