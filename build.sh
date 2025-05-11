f50e833c69d1b9635286712091266c5a4819952b
blueback <bluebackpandey@gmail.com>
2025-05-11 08:51:08 +0000

    build.sh refactoring

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
-export JAVA_HOME=jdk-24.0.1
-export ANDROID_HOME=cmdline-tools/
+export JAVA_HOME=${PWD}/third_party/jdk-24.0.1
+export ANDROID_HOME=${PWD}/third_party/cmdline-tools/
 export PATH=${PATH}:${ANDROID_HOME}/enumlator
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/tools/bin
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
-#cmdline-tools/bin/sdkmanager --install "platform-tools" "platforms;android-34" "build-tools;34.0.0" --sdk_root=cmdline-tools
-#cmdline-tools/bin/sdkmanager --install "gradle" --sdk_root=cmdline-tools
+#./third_party/cmdline-tools/bin/sdkmanager --install "platform-tools" "platforms;android-34" "build-tools;34.0.0" --sdk_root=cmdline-tools
+#./third_party/cmdline-tools/bin/sdkmanager --install "gradle" --sdk_root=cmdline-tools
 
-#gradle-8.14/bin/gradle build --stacktrace --info --scan --debug
-gradle-8.14/bin/gradle build
+#./third_party/gradle-8.14/bin/gradle build --stacktrace --info --scan --debug
+rm -rf .gradle
+rm -rf build
+rm -rf app/build
+./third_party/gradle-8.14/bin/gradle build
 
